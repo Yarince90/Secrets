ea7475db3498c1041545ae9982b52d86023c9993
Yarince Vigil <Yarincevigil@hotmail.com>
2022-07-07 18:59:26 -0400

    Implemented Oauth for google registrastion and login as well as new button style classes from bootstrap for google registration and login - currently need to fix invalid local password not re-directing to login and giving Unautharized error page instead

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 const port = 3000;
@@ -29,19 +31,42 @@ mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
+    googleID: String,
     active: Boolean
 });
 
 //Method for hashing and salting
 userSchema.plugin(passportLocalMongoose);
+//oauth plugin for signing in with google acc
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
 
 //Only necessary when using sessions
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/',(req, res)=>{
     res.render('home');
@@ -64,6 +89,16 @@ app.get('/secrets', (req, res)=>{
    }
 });
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets page.
+    res.redirect('/secrets');
+  });
+
 app.get('/logout', function(req, res, next) {
     req.logout(function(err) {
       if (err){ 
@@ -83,7 +118,7 @@ app.post('/register', (req, res)=>{
             res.redirect('/register');
          }
          else{
-            passport.authenticate("local")(req, res, function(){
+            passport.authenticate('local')(req, res, function(){
                 res.redirect('/secrets');
             });
          }
@@ -101,23 +136,12 @@ app.post('/login', (req, res)=>{
             res.redirect('/login');
         }
         else{
-            passport.authenticate("local");
-            res.redirect('/secrets');
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/secrets');
+            });
         }
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.listen(port, ()=>{
