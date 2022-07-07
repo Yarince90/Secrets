fae776f34ea54a994a8b8b714430bddcf3a66ca8
Yarince Vigil <Yarincevigil@hotmail.com>
2022-07-07 16:47:31 -0400

    replaced Authentication from bcrypt to Passport, created cookies for user sessions and implemented logout functionality to end sessions

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-//const encrypt = require('mongoose-encryption');
 const ejs = require('ejs');
 
 const app = express();
@@ -13,18 +13,35 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+app.use(passport.initialize());
+app.use(passport.session());
+
+//DB Connection
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
 
+//User Schema
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    active: Boolean
 });
 
-//Mongoose Encryption 
-//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+//Method for hashing and salting
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model('User', userSchema);
 
+passport.use(User.createStrategy());
+
+//Only necessary when using sessions
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/',(req, res)=>{
     res.render('home');
@@ -38,41 +55,54 @@ app.get('/register',(req, res)=>{
     res.render('register');
 });
 
+app.get('/secrets', (req, res)=>{
+   if(req.isAuthenticated()){
+    res.render('secrets');
+   } 
+   else{
+    res.redirect('/login');
+   }
+});
+
+app.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err){ 
+        return next(err); 
+        }
+        else{
+            res.redirect('/');
+        }
+    });
+  });
+
 app.post('/register', (req, res)=>{
     
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        
-        newUser.save((err)=>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.render('secrets');
-            }
-        });
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect('/register');
+         }
+         else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect('/secrets');
+            });
+         }
     });
 });
 
 app.post('/login', (req, res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, (err, foundUser)=>{
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
         if(err){
             console.log(err);
+            res.redirect('/login');
         }
         else{
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                   if(result){
-                    res.render('secrets');
-                   }
-                });
-            }
+            passport.authenticate("local");
+            res.redirect('/secrets');
         }
     });
 });
